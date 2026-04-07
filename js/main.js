94f859b460f51f36acc984bd45601925a23ab4da
zee1013 <dmswl1251@gmail.com>
2026-04-07 13:16:46 +0900

    메인 비주얼 스와이프 수정

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 const swiper = new Swiper(".visual-swiper", {
-    spaceBetween: 30,
-      centeredSlides: true,
-      autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-      },
+//   spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: ".swiper-pagination",
     dynamicBullets: true,
